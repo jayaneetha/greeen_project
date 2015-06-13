b307a0c54a971cb6c35a76863287d3ada4829a62
Thejan Rajapakshe <coder.clix@gmail.com>
2015-06-14 00:31:39 +0530

    Added Ajax API

--- application/views/login.php
+++ application/views/login.php
@@ -1,24 +1,31 @@
-
-<?php 
+<?php
 $this->load->view('partial/header');
 ?>
-    </head>
-    <body>
-        <div class="container">
-            <div style="margin-top: 120px" class="row clearfix">
-                <div class="col-lg-4"></div>
-                <div class="col-lg-4 well">
-                    <center><h1 class="h1">Sign in</h1></center>
-                    <form action="<?php echo site_url('user/login'); ?>" method="POST">
-                        <label for="username">Phone Number</label>
-                        <input class="form-control" type="text" name="username" placeholder="Username"/>
-                        <label for="password">Password</label>
-                        <input class="form-control" type="password" name="password" placeholder="Password"/>
-                        <input class="btn btn-success" type="submit" value="Login"/>
-                        <a class="btn btn-info pull-right" href="<?php echo base_url('user/register_collector'); ?>">Register</a>
-                    </form>
-                </div>
-            </div>
+</head>
+<body>
+<div class="container">
+    <div style="margin-top: 80px" class="row clearfix">
+        <div class="col-lg-8">
+            <h1>Greeen Project </br>
+                <small>Collector Login</small>
+            </h1>
+            <img src="<?php echo base_url('assests/images/login-background.png'); ?>"
+                 class="img-responsive pull-right"/>
         </div>
-    </body>
+        <div class="col-lg-4 well" style="margin-top: 150px">
+            <center><h1 class="h1">Sign in</h1></center>
+            <form action="<?php echo site_url('user/login'); ?>" method="POST">
+                <label for="username">Phone Number</label>
+                <input class="form-control" type="text" name="username" placeholder="Username"/>
+                <label for="password">Password</label>
+                <input class="form-control" type="password" name="password" placeholder="Password"/>
+                <br>
+                <input class="btn btn-success" type="submit" value="Login"/>
+                <a class="btn btn-info pull-right"
+                   href="<?php echo base_url('user/register_collector'); ?>">Register</a>
+            </form>
+        </div>
+    </div>
+</div>
+</body>
 </html>
