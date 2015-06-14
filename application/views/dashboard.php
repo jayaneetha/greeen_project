0b557e7145885ca5eae5b412d1f22732d7da5f41
Thejan Rajapakshe <coder.clix@gmail.com>
2015-06-14 08:16:58 +0530

    Finetunes

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -56,18 +56,20 @@ $this->load->view('partial/header');
                     </div>
                 </div>
             </div>
+
             <table id="waste_locations" class="table table-responsive table-bordered table-hover">
                 <thead>
                 <td class="text-center">Type</td>
                 <td class="text-center">Date</td>
                 <td class="text-center">Time</td>
-                <td class="text-center" style="width: 225px;">Address</td>
+                <td class="text-center" style="width: 118px;">Address</td>
                 <td class="text-center">Action</td>
                 </thead>
                 <tbody>
 
                 </tbody>
             </table>
+
         </div>
         <div class="col-lg-5 col-md-5">
 
